71bcb012fe32383b3567d8a73c483b66acf68862
Ferdi <hello@ferdi.digital>
2025-12-13 00:24:12 +0100

    fix: update scrollBehavior function to remove unused parameters

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,7 +1,7 @@
 import type { RouterConfig } from "@nuxt/schema";
 
 export default <RouterConfig>{
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior() {
     // Scroll the main element to top on route change
     const main = document.querySelector("main");
     if (main) {
